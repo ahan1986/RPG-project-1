0f269a93d28440932ec139b4a62cf925cc42d39a
Sutton Ashby <korim_fidelis@yahoo.com>
2018-05-16 12:25:06 -0400

    basica game object structure

---
+++ public/js/game.js
@@ -0,0 +1,31 @@
+var game = {
+    player: undefined,
+    opponent: undefined,
+    playerChoice: undefined,
+    opponentsFought: [],
+    initalize: function(){
+        getPlayer()
+        getOpponent()
+        setLoss()
+        combatLoop()
+    },
+    getPlayer: function(){
+
+    },
+    getOpponent: function(){
+
+    },
+    setLoss: function(){
+
+    },
+    combatLoop: function(){
+
+    },
+    getPlayerChoice: function(){
+
+    },
+    computerChoice: function(){
+
+    }
+
+}
