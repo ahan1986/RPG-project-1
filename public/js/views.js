91f5fe728a0a9d09c9ee12ed63b25920d708e810
Joseph Holder <josephholder@Josephs-MacBook-Pro.local>
2018-05-19 11:57:04 -0400

    create JS

--- public/js/views.js
+++ public/js/views.js
@@ -1,7 +1,64 @@
-// User name and pw validation
+//login JS
+
+
+
+
+
+
+
+
+
+
+
+//create warrior JS
+$("#pwAlert").hide();
+$("#skillsAlertUnder").hide();
+$("#skillsAlertOver").hide();
+
 var username = "";
 var password = "";
 var password2 = "";
+
+function validations() {
+    var x = false;
+    var y = false;
+    var z = false;
+
+    if (password != password2) {
+        $("#pwAlert").show();
+    } else {
+        x = true;
+    }
+    if (speed + health + strength > 15) {
+        $("#skillsAlertOver").hide();
+    } else {
+        y = true;
+    }
+    if (speed + health + strength < 15) {
+        $("#skillsAlertUnder").hide();
+    } else {
+        z = true;
+    }
+    if (x && y && z){
+        sendUser();
+    }
+};
+
+function sendUser() {
+    var user = {
+        username: username,
+        password: password,
+        avatarID: warrior,
+        health: health,
+        speed: speed,
+        strength: strength,
+    }
+    $.ajax({
+        method: "POST",
+        url: "/api/user",
+        data: user
+    });
+};
 
 $("#cwSubmit").click(function () {
     event.preventDefault();
@@ -9,7 +66,10 @@ $("#cwSubmit").click(function () {
     password = $("#password").val().trim();
     password2 = $("#password2").val().trim();
     warrior = $("input[type='radio'][name='warrior']:checked").val();
+    speed = $("#speed").val();
+    health = $("#health").val();
+    strength = $("#strength").val();
 
-   console.log();
+    validations()
 });
 
