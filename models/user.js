2fe3eaa2d172fcdf8275b247babd66a8d4b2fae7
Handrew <andrew.a.han@gmail.com>
2018-05-22 18:09:26 -0400

    updating to current master

--- models/user.js
+++ models/user.js
@@ -2,11 +2,31 @@ module.exports = function(sequelize, DataTypes){
     var User = sequelize.define("User", {
         username: {
             type: DataTypes.STRING,
-            allowNull: false
+            allowNull: false,
+            unique: true,
+            validate: {
+                isUnique: function(value, next) {
+                    var self = this;
+                    User.find({where: {username: value}})
+                        .then(function(user) {
+                            if(user && self.id !== user.id) {
+                                return next('Username already in use!');
+                            }
+                            return next();
+                        })
+                        .catch(function(err) {
+                            return next(err);
+                        });
+                }
+            }
         },
         password: {
             type: DataTypes.STRING,
-            allowNull: false
+            allowNull: false,
+            unique: {
+                args: true,
+                msg: "ERR-PASS"
+            }
         },
         avatarID: {
             type: DataTypes.INTEGER,
